e725c3197a7df1d438f1ec0ca230b09df0facf2f
jukka <jraappan@yahoo.com>
2019-04-14 13:40:50 +0300

    Added more comments and logging.

--- morse.py
+++ morse.py
@@ -7,7 +7,8 @@ import logging
 
 logging.basicConfig(filename='morse.log', level=logging.DEBUG)
 
-morse_table = {
+# morse key table for encoding and decoding
+morse_key_table = {
                     "a": u"\u2022 \u2013",
                     "b": u"\u2013 \u2022 \u2022 \u2022",
                     "c": u"\u2013 \u2022 \u2013 \u2022",
@@ -63,8 +64,8 @@ def encode_to_morse(text_to_morse):
     morse_result = ""
     if text_to_morse:
         for letter in text_to_morse:
-            if letter.lower() in morse_table:  # checks if letter in code table
-                morse_result_list.append(morse_table[letter.lower()].encode('utf-8'))
+            if letter.lower() in morse_key_table:  # checks if letter in code table
+                morse_result_list.append(morse_key_table[letter.lower()].encode('utf-8'))
                 if letter.lower() == '.':
                     morse_result_list.append('\n ')
             else:
@@ -74,79 +75,93 @@ def encode_to_morse(text_to_morse):
                     logging.debug("encode_to_morse / not valid letter %s" % letter.encode('hex'))
 
         morse_result = ".".join(morse_result_list)  # adds dot to separate the morse marks
-    return morse_result
+
+    return morse_result  # returns encoded morse string
 
 
 def decode_to_text(morse_to_text):
     """
-    decode_to_text() -function decodes given morse strin to text string
+    decode_to_text() -function decodes given morse string to text string
     :param morse_to_text: morse string
     :return: text_result.upper(): text string converted to UPPER CASE
     """
     logging.info("decode_to_text()")
     text_result = ""
-    tmp_morse_table = {}
+    tmp_morse_key_table = {}
     if morse_to_text:
-        test = morse_to_text.split('.')  # makes list about the morse string
-        for key in morse_table:
-            new_key = repr(morse_table[key].encode('utf-8'))
-            tmp_morse_table[new_key] = key
+        morse_list = morse_to_text.split('.')  # makes list about the morse string
+        for key in morse_key_table:  # creates morse table so we get decoding table
+            new_key = repr(morse_key_table[key].encode('utf-8'))  # gets hex value of morse marks
+            tmp_morse_key_table[new_key] = key  # saves letter as value
 
-        for code in test:
-            if repr(code) in tmp_morse_table:
-                text_result = text_result + tmp_morse_table[repr(code)]
-                if tmp_morse_table[repr(code)] == '.':
-                    text_result = text_result + '\n'
+        for code in morse_list:  # decodes morse to letter
+            if repr(code) in tmp_morse_key_table:  # checks key value
+                text_result = text_result + tmp_morse_key_table[repr(code)]
+                if tmp_morse_key_table[repr(code)] == '.':  # checks dot
+                    text_result = text_result + '\n'  # adds new line in to text
             elif code == ' ':
-                text_result = text_result + ' '
+                text_result = text_result + ' '  # adds white space in to text
             else:
                 logging.debug("decode_to_text / not valid mark %s" % code.encode('hex'))
 
-    return text_result.upper()
+    return text_result.upper()  # returns decoded text string in UPPER case
 
 
 def get_sample_text(text_file):
-    print "***get_sample_text"
+    """
+    get_sample_text() -function opens the given text file and save the text content to string variable
+    :param text_file: text file path name
+    :return: sample_text: content of text file as string
+    """
+    logging.info("get_sample_text()")
     sample_text = ""
     try:
-        with open(text_file, "r") as r:
-            sample_text = r.read()
-            print sample_text
+        with open(text_file, "r") as r:  # opens the file to read
+            sample_text = r.read()  # saves file content
     except EnvironmentError as e:
-        print e
-    return sample_text
+        logging.debug("get_sample_text / error in reading the file {!s}".format(e))
+    return sample_text  # returns content of file as string
 
 
 def get_sample_morse(test_file):
-    print "***get_sample_morse"
+    """
+    get_sample_morse() -function opens the given morse file and save the content to string variable
+    :param test_file: morse file path name
+    :return: content of morse file as string
+    """
+    logging.info("get_sample_morse()")
     sample_morse = ""
     try:
-        with open(test_file, "r") as r:
-            sample_morse = r.read()
-            print sample_morse
+        with open(test_file, "r") as r:  # open the file read
+            sample_morse = r.read()  # saves file content
     except EnvironmentError as e:
-        print e
+        logging.debug("get_sample_morse / error in reading the file {!s}".format(e))
 
-    return sample_morse
+    return sample_morse  # returns content of file as string
 
 
 def save_result(file_name, result_item):
-    print "***save_result"
+    """
+    save_result() -function saves encoded/decoded string result in the file
+    :param file_name: Morse.txt or Text.txt
+    :param result_item: string to save in to file
+    :return: status: True in success, False in failure
+    """
+    logging.info("save_result()")
     status = True
     try:
-        with open(file_name, "wb") as w:
-            w.write(str(result_item))
-            print "File saved"
+        with open(file_name, "w") as w:  # opens the file to write
+            w.write(str(result_item))  # writes the content to file
     except Exception as e:
-        print "save_result/error %s" % e
+        logging.debug("save_result / error in writing the file {!s}".format(e))
         status = False
 
-    return status
+    return status  # returns status info
 
 
 def simple_cli():
     """
-    simple_cli() is simle command line interface
+    simple_cli() is simple command line interface
     :return: test_file: file to encode/decode,
              user_selection: user selection (1-3), see below
     """
@@ -169,27 +184,37 @@ def simple_cli():
         print "Goodbye"
         sys.exit()
 
-    return test_file, user_selection
+    return test_file, user_selection  # returns file path name and user selection
 
 
 def main():
+    """
+    main() -function implements encoding/decoding the file content given by user using by sub functions
+    """
     while True:
-        test_file, selection = simple_cli()
-        if test_file and selection == str(1):
-            text_to_morse = get_sample_text(test_file)  # Get the sample text
+        test_file, selection = simple_cli()  # simple user CLI interface
+        if test_file and selection == str(1):  # checks text file and use selection
+            text_to_morse = get_sample_text(test_file)  # gets the sample text
             if text_to_morse:
-                morse_result = encode_to_morse(text_to_morse)  # Encode the sample to morse
-                status = save_result("Morse.txt", morse_result)  # Save result to file
-                print status
+                morse_result = encode_to_morse(text_to_morse)  # encodes the sample to morse
+                print "Text to morse: \n %s" % morse_result
+                status = save_result("Morse.txt", morse_result)  # saves result to file
+                if status:
+                    print "File saved"
+                else:
+                    print "Error in file saving"
             else:
                 print "No text to encode"
-        elif test_file and selection == str(2):
-            print "*** morse to text"
-            morse_to_text = get_sample_morse(test_file)  # Get sample morse
+        elif test_file and selection == str(2):  # checks morse file and use selection
+            morse_to_text = get_sample_morse(test_file)  # get sample morse
             if morse_to_text:
-                text_result = decode_to_text(morse_to_text)  # Decode the sample to text
-                print "text_result %s" % text_result
-                status = save_result("Text.txt", text_result)  # Save result to file
+                text_result = decode_to_text(morse_to_text)  # decodes the sample to text
+                print "Morse to text: \n %s" % text_result
+                status = save_result("Text.txt", text_result)  # saves result to file
+                if status:
+                    print "File saved"
+                else:
+                    print "Error in file saving"
             else:
                 print "No morse to decode"
 
